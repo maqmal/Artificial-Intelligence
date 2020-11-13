b3426964d368ed91d17fb5710a40123489bd67ea
aqmal pangestu <pangestuaqmal@gmail.com>
2020-11-13 16:42:26 +0700

    init

---
+++ main.py
@@ -0,0 +1,157 @@
+import pandas as pd 
+
+def trapesium(x,a,b,c,d): # Fungsi keanggotaan trapesium
+    if ((x<=a) or (x>=d)):
+        nilai = 0
+    elif ((x>a) and (x<b)):
+        nilai = (x-a)/(b-a)
+    elif ((x>=b) and (x<=c)):
+        nilai = 1
+    elif ((x>c) and (x<=d)):
+        nilai = -(x-d)/(d-c)
+
+    return nilai
+
+def rendah(x,nilai_fuzzy): # Variabel linguistik
+    a = 0.0
+    b = 0.0
+    c = 5.0
+    d = 8.0
+    nilai_fuzzy['rendah'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def sedang(x,nilai_fuzzy): # Variabel linguistik
+    a = 5.0
+    b = 8.0
+    c = 10.0
+    d = 13.0
+    nilai_fuzzy['sedang'] = round(trapesium(x,a,b,c,d),2)
+    
+    return nilai_fuzzy
+
+def tinggi(x,nilai_fuzzy): # Variabel linguistik
+    a = 10.0
+    b = 13.0
+    c = 20 # lebih dari nilai maksimum penghasilan data mahasiswa agar tidak 0
+    d = 20 # lebih dari nilai maksimum penghasilan data mahasiswa agar tidak 0
+    nilai_fuzzy['tinggi'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def sedikit(x,nilai_fuzzy): # Variabel linguistik
+    a = 0.0
+    b = 0.0
+    c = 2.0
+    d = 3.0
+    nilai_fuzzy['sedikit'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def cukup(x,nilai_fuzzy): # Variabel linguistik
+    a = 2.0
+    b = 3.0
+    c = 4.0
+    d = 5.0
+    nilai_fuzzy['cukup'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def banyak(x,nilai_fuzzy): # Variabel linguistik
+    a = 4.0
+    b = 5.0
+    c = 6.0
+    d = 8.0
+    nilai_fuzzy['banyak'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def sangat_banyak(x,nilai_fuzzy): # Variabel linguistik
+    a = 6.0
+    b = 8.0
+    c = 12.0 # lebih dari nilai maksimum pengeluaran data mahasiswa agar tidak 0
+    d = 12.0 # lebih dari nilai maksimum pengeluaran data mahasiswa agar tidak 0
+    nilai_fuzzy['sangat_banyak'] = round(trapesium(x,a,b,c,d),2)
+
+    return nilai_fuzzy
+
+def fuzzification(pengeluaran,penghasilan):
+    fuzzy_penghasilan = {}
+    fuzzy_pengeluaran = {}
+    i = 1
+    x = 1
+    for data in penghasilan.values:
+        fuzzy = {}
+        fuzzy_penghasilan[i] = rendah(float(data), fuzzy)
+        fuzzy_penghasilan[i] = sedang(float(data), fuzzy)
+        fuzzy_penghasilan[i] = tinggi(float(data), fuzzy)
+        i+=1
+    for data in pengeluaran.values:
+        fuzzy = {}
+        fuzzy_pengeluaran[x] = sedikit(float(data), fuzzy)
+        fuzzy_pengeluaran[x] = cukup(float(data), fuzzy)
+        fuzzy_pengeluaran[x] = banyak(float(data), fuzzy)
+        fuzzy_pengeluaran[x] = sangat_banyak(float(data), fuzzy)
+        x+=1
+
+    return fuzzy_pengeluaran,fuzzy_penghasilan
+
+def inference(nilai_pengeluaran,nilai_penghasilan):
+    nilai_kelayakan = {}
+    i = 1
+    while i <=100:
+        layak = {}
+        if ((nilai_penghasilan[i]['rendah']!=0) and (nilai_pengeluaran[i]['sedikit']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['rendah'],nilai_pengeluaran[i]['sedikit'])
+
+        if ((nilai_penghasilan[i]['rendah']!=0) and (nilai_pengeluaran[i]['cukup']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['rendah'],nilai_pengeluaran[i]['cukup'])
+
+        if ((nilai_penghasilan[i]['rendah']!=0) and (nilai_pengeluaran[i]['banyak']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['rendah'],nilai_pengeluaran[i]['banyak'])
+
+        if ((nilai_penghasilan[i]['rendah']!=0) and (nilai_pengeluaran[i]['sangat_banyak']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['rendah'],nilai_pengeluaran[i]['sangat_banyak'])
+
+        if ((nilai_penghasilan[i]['sedang']!=0) and (nilai_pengeluaran[i]['sedikit']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['sedang'],nilai_pengeluaran[i]['sedikit'])
+
+        if ((nilai_penghasilan[i]['sedang']!=0) and (nilai_pengeluaran[i]['cukup']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['sedang'],nilai_pengeluaran[i]['cukup'])
+
+        if ((nilai_penghasilan[i]['sedang']!=0) and (nilai_pengeluaran[i]['banyak']!=0)):
+            layak['ditolak'] = min(nilai_penghasilan[i]['sedang'],nilai_pengeluaran[i]['banyak'])
+
+        if ((nilai_penghasilan[i]['sedang']!=0) and (nilai_pengeluaran[i]['sangat_banyak']!=0)):
+            layak['ditolak'] = min(nilai_penghasilan[i]['sedang'],nilai_pengeluaran[i]['sangat_banyak'])
+
+        if ((nilai_penghasilan[i]['tinggi']!=0) and (nilai_pengeluaran[i]['sedikit']!=0)):
+            layak['ditolak'] = min(nilai_penghasilan[i]['tinggi'],nilai_pengeluaran[i]['sedikit'])
+
+        if ((nilai_penghasilan[i]['tinggi']!=0) and (nilai_pengeluaran[i]['cukup']!=0)):
+            layak['ditolak'] = min(nilai_penghasilan[i]['tinggi'],nilai_pengeluaran[i]['cukup'])
+
+        if ((nilai_penghasilan[i]['tinggi']!=0) and (nilai_pengeluaran[i]['banyak']!=0)):
+            layak['ditolak'] = min(nilai_penghasilan[i]['tinggi'],nilai_pengeluaran[i]['banyak'])
+
+        if ((nilai_penghasilan[i]['tinggi']!=0) and (nilai_pengeluaran[i]['sangat_banyak']!=0)):
+            layak['diterima'] = min(nilai_penghasilan[i]['tinggi'],nilai_pengeluaran[i]['sangat_banyak'])
+        
+        nilai_kelayakan[i] = layak
+        i+=1
+    return nilai_kelayakan
+
+def defuzzification(nilai_kelayakan):
+    
+
+if __name__=="__main__":
+    data_mahasiswa = pd.read_excel('./Mahasiswa.xls')
+    penghasilan = pd.DataFrame(data_mahasiswa, columns=['Penghasilan'])
+    pengeluaran = pd.DataFrame(data_mahasiswa, columns=['Pengeluaran'])
+    
+    nilai_pengeluaran, nilai_penghasilan = fuzzification(pengeluaran, penghasilan)
+    nilai_kelayakan = inference(nilai_pengeluaran,nilai_penghasilan)
+    hasil = defuzzification(nilai_kelayakan)
+    print(hasil)
+    
+    
